a9d900ed5bcd5b79967ec01c7712806605f769e1
TannerGruebele <tanner@saambaa.com>
2017-08-03 14:08:24 -0700

    New repo

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES
+     ('Bacon CB'),
+     ('Turkey Burger'),
+     ('Hawaian Burger'),
+     ('Deluxe Burger');source schema.sqlhel
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var port = process.env.PORT || 8080;
+var app = express();
+
+
+
+// app.use(express.static("public"));
+app.use(express.static("public/assets"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function() {
+    console.log("App listening on port " + port);
+});
