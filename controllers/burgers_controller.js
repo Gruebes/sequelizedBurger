5ec35ef2d0f577fb1873ef4ecd90f641a057fc95
TannerGruebele <tanner@saambaa.com>
2017-08-04 22:54:15 -0700

    sequelized

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,49 +1,96 @@
 var express = require("express");
-
 var router = express.Router();
 
-var burger = require("../models/burger.js");
+var db = require("../models");
+
 
 router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
+    // burger.selectAll(function(data) {
+    //     var hbsObject = {
+    //         burgers: data
+    //     };
+    //     console.log(hbsObject);
+    //     res.render("index", hbsObject);
+    // });
+
+
+    db.burger.findAll({}).then(function(data) {
+        console.log('------------------------------------');
+        console.log(data);
+        console.log('------------------------------------');
+
         var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
         res.render("index", hbsObject);
     });
+
+
 });
 
 router.post("/", function(req, res) {
-    burger.insertOne([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function() {
-        res.redirect("/");
-    });
+    // burger.insertOne([
+    //     "burger_name", "devoured"
+    // ], [
+    //     req.body.burger_name, req.body.devoured
+    // ], function() {
+    //     res.redirect("/");
+    // });
+
+    db.burger.create({
+            burger_name: req.body.burger_name,
+            devoured: req.body.devoured
+        })
+        .then(function() {
+            res.redirect("/");
+        });
+
+
 });
 
 router.put("/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+    // var condition = "id = " + req.params.id;
 
-    console.log("condition", condition);
+    // console.log("condition", condition);
 
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function() {
-        res.redirect("/");
-    });
+    // burger.updateOne({
+    //     devoured: req.body.devoured
+    // }, condition, function() {
+    //     res.redirect("/");
+    // });
+
+
+    db.burger.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function() {
+            res.redirect("/");
+        });
+
+
 });
 
 router.delete("/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    console.log('------------------------------------');
-    console.log("deleting");
-    console.log('------------------------------------');
-    burger.deleteOne(condition, function() {
-        res.redirect("/");
-    });
+    // var condition = "id = " + req.params.id;
+    // console.log('------------------------------------');
+    // console.log("deleting");
+    // console.log('------------------------------------');
+    // burger.deleteOne(condition, function() {
+    //     res.redirect("/");
+    // });
+
+
+    db.burger.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function() {
+            res.redirect("/");
+        });
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
+var db = require("./models");
+
 
 var port = process.env.PORT || 8080;
 var app = express();
-
 
 
 // app.use(express.static("public"));
@@ -25,6 +26,12 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port, function() {
-    console.log("App listening on port " + port);
+// app.listen(port, function() {
+//     console.log("App listening on port " + port);
+// });
+
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+        console.log("App listening on PORT " + port);
+    });
 });
