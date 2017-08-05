5f32c494d19955b1e095c9af755cba05d8216fc2
TannerGruebele <tanner@saambaa.com>
2017-08-04 23:23:05 -0700

    Fixed model bug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,20 +5,7 @@ var db = require("../models");
 
 
 router.get("/", function(req, res) {
-    // burger.selectAll(function(data) {
-    //     var hbsObject = {
-    //         burgers: data
-    //     };
-    //     console.log(hbsObject);
-    //     res.render("index", hbsObject);
-    // });
-
-
     db.burger.findAll({}).then(function(data) {
-        console.log('------------------------------------');
-        console.log(data);
-        console.log('------------------------------------');
-
         var hbsObject = {
             burgers: data
         };
@@ -29,14 +16,6 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-    // burger.insertOne([
-    //     "burger_name", "devoured"
-    // ], [
-    //     req.body.burger_name, req.body.devoured
-    // ], function() {
-    //     res.redirect("/");
-    // });
-
     db.burger.create({
             burger_name: req.body.burger_name,
             devoured: req.body.devoured
@@ -44,22 +23,9 @@ router.post("/", function(req, res) {
         .then(function() {
             res.redirect("/");
         });
-
-
 });
 
 router.put("/:id", function(req, res) {
-    // var condition = "id = " + req.params.id;
-
-    // console.log("condition", condition);
-
-    // burger.updateOne({
-    //     devoured: req.body.devoured
-    // }, condition, function() {
-    //     res.redirect("/");
-    // });
-
-
     db.burger.update(req.body, {
             where: {
                 id: req.params.id
@@ -68,20 +34,9 @@ router.put("/:id", function(req, res) {
         .then(function() {
             res.redirect("/");
         });
-
-
 });
 
 router.delete("/:id", function(req, res) {
-    // var condition = "id = " + req.params.id;
-    // console.log('------------------------------------');
-    // console.log("deleting");
-    // console.log('------------------------------------');
-    // burger.deleteOne(condition, function() {
-    //     res.redirect("/");
-    // });
-
-
     db.burger.destroy({
             where: {
                 id: req.params.id
@@ -90,7 +45,6 @@ router.delete("/:id", function(req, res) {
         .then(function() {
             res.redirect("/");
         });
-
 });
 
 module.exports = router;
